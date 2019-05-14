6a70750ea65436b8718d935d6ec8fbe6452ada9c
Rodrigo Pina Lozano (CI/DAD5.1) <rodrigo.pina@de.bosch.com>
2019-05-14 19:09:40 +0200

    Connects sensor values to UI Labels

--- mobile-app/bettercrops/app/browse/browse-page.ts
+++ mobile-app/bettercrops/app/browse/browse-page.ts
@@ -1,27 +1,37 @@
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
 import { BrowseViewModel } from "./browse-view-model";
+import { EventData } from "tns-core-modules/data/observable";
+import { getViewById } from "tns-core-modules/ui/core/view";
 import { Label } from "tns-core-modules/ui/label";
-
+import { View } from "tns-core-modules/ui/core/view";
 
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
     page.bindingContext = new BrowseViewModel();
-    const capacitorLabel = page.getViewById("capacitorLabel");
 }
 
 
-export function getScan(){
+export function getScan(args: EventData){
     fetch("http://100.100.149.140/iolink/v1/devices/1/processdata/value?format=raw&direction=both")
     .then((response) => response.json())
     .then((capacitorResponse) => {
         //console.log("Capacitor Response ******** "+ JSON.stringify(capacitorResponse));
-        console.log("Capacitor Value: " + capacitorResponse.in.value.deviceRawTypeValue);
-        this.capacitorValue = capacitorResponse.in.value.deviceRawTypeValue;
+        var capacitorValue = capacitorResponse.in.value.deviceRawTypeValue;
+        console.log("Capacitor Value: " + capacitorValue);
+
+        let button = <View>args.object;
+        let parent = button.parent;
+        let capacitorlbl = <Label>getViewById(parent, "capacitorLabel");
+        capacitorlbl.text = capacitorValue;
+
         fetch("http://100.100.149.140/iolink/v1/devices/3/processdata/value?format=raw&direction=both")
         .then((rfidresponse) => rfidresponse.json())
         .then((rfidJson) => {
             //console.log("RFID Response ******** "+ JSON.stringify(rfidJson));
             console.log("RFID Value: " + rfidJson.in.value.deviceRawTypeValue);
+            var rfidValue = rfidJson.in.value.deviceRawTypeValue;
+            let rfidlbl = <Label>getViewById(parent, "rfidLabel");
+            rfidlbl.text = rfidValue;
         }).catch((e) => {
             console.error("Error reading RFID");
             console.error(e);
