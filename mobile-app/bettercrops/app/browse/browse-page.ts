3c2a7b1009e4d15e337b3ffa9812dbf89329bd40
Rodrigo Pina Lozano (CI/DAD5.1) <rodrigo.pina@de.bosch.com>
2019-05-14 18:17:26 +0200

    Connects sensors to App

--- mobile-app/bettercrops/app/browse/browse-page.ts
+++ mobile-app/bettercrops/app/browse/browse-page.ts
@@ -1,25 +1,33 @@
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
 import { BrowseViewModel } from "./browse-view-model";
+import { Label } from "tns-core-modules/ui/label";
+
 
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
     page.bindingContext = new BrowseViewModel();
+    const capacitorLabel = page.getViewById("capacitorLabel");
 }
 
 
 export function getScan(){
     fetch("http://100.100.149.140/iolink/v1/devices/1/processdata/value?format=raw&direction=both")
-    .then((response) => response.text())
-    .then((r) => {
-        console.log("TEST capacitor ******** "+ r);
+    .then((response) => response.json())
+    .then((capacitorResponse) => {
+        //console.log("Capacitor Response ******** "+ JSON.stringify(capacitorResponse));
+        console.log("Capacitor Value: " + capacitorResponse.in.value.deviceRawTypeValue);
+        this.capacitorValue = capacitorResponse.in.value.deviceRawTypeValue;
+        fetch("http://100.100.149.140/iolink/v1/devices/3/processdata/value?format=raw&direction=both")
+        .then((rfidresponse) => rfidresponse.json())
+        .then((rfidJson) => {
+            //console.log("RFID Response ******** "+ JSON.stringify(rfidJson));
+            console.log("RFID Value: " + rfidJson.in.value.deviceRawTypeValue);
+        }).catch((e) => {
+            console.error("Error reading RFID");
+            console.error(e);
+        });
     }).catch((e) => {
+        console.error("Error reading Capacitor")
+        console.error(e);
     });
-
-    fetch("http://100.100.149.140/iolink/v3/devices/1/processdata/value?format=raw&direction=both")
-    .then((response) => response.text())
-    .then((r) => {
-        console.log("TEST RFID ******** "+ r);
-    }).catch((e) => {
-    });
-
 }
