b0ef65c37aaa01892658754e9068a5360f09ba2a
Tilman Linden <tilman@monoqi.de>
2019-05-14 18:05:53 +0200

    Journey API accepts valid transaction.

--- bettercrops-api/api.js
+++ bettercrops-api/api.js
@@ -7,6 +7,8 @@ const validator = new Validator({allErrors: true})
 
 router.use(bodyParser.json())
 
+// Journey -------------------------------------------------------------------------------------------------------------
+
 router.get('/journey/:bagId', (req, res) => {
     res
         .status(200)
@@ -19,11 +21,18 @@ router.post('/journey/:bagId/reset', (req, res) =>  {
         .json({ error: 'Not implemented' })
 })
 
+// Transaction ---------------------------------------------------------------------------------------------------------
+
 router.post('/transaction', (req, res) => {
     res
-        .status(500)
-        .json({ error: 'Not implemented' })
+        .status(201)
+        .json({
+            txId: '12345',
+            journeyId: '12345'
+        })
 })
+
+// Quality -------------------------------------------------------------------------------------------------------------
 
 router.put('/quality/check', (req, res) => {
     res
--- bettercrops-api/api.spec.js
+++ bettercrops-api/api.spec.js
@@ -16,3 +16,30 @@ describe('GET /journey/:bagId', () => {
         expect(res.body).toEqual([])
     })
 })
+
+describe('POST /transaction', () => {
+    test('it should accept a valid transaction', async () => {
+        const app = initApi()
+        const tx = {
+            bagId: '09-093-938409',
+            timestamp: '2084-04-23T18:25:43.511Z',
+            measurement: {
+                capacity: 1837
+            },
+            price: {
+                amount: 100.32,
+                currency: 'USD'
+            },
+            sellerId: '0298/3847390',
+            buyerId: '0293/2983749',
+            scannerId: '39300-3948-38'
+        }
+        const res = await request(app).post('/transaction')
+        expect(res.statusCode).toEqual(201)
+        expect(res.body.txId).toBeDefined
+        expect(res.body.journeyId).toBeDefined
+    })
+
+    // Create tx ID
+    // Create journey ID
+})
